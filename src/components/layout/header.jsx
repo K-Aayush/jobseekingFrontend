9e9b37583650b5f3654e432e3686225bb1a52766
Aayush <ayushkarkee6@gmail.com>
2024-06-28 18:50:01 +0545

    imported required libraries and made logout get function

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,4 +1,27 @@
+import { useContext, useState } from "react";
+import { Context } from "../../main";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
+
 const Header = () => {
+  const { isAuthorized, setIsAuthorized, user } = useContext(Context);
+
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      const response = await axios.get("/base-url/user/logout", { withCredentials: true });
+      toast.success(response.data.message);
+      setIsAuthorized(false);
+      navigate("/");
+    }
+    catch (error) {
+      toast.error(error.response.data.message);
+      setIsAuthorized(true);
+    }
+  }
+
   return (
     <div>Header</div>
   )
